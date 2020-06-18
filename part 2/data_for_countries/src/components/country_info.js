fed80c76de8d982575aa1bae9f0f3e0958dc7ef9
Winston <winstonyeo99@yahoo.com>
2020-06-18 11:22:43 +0800

    Complete exercise 2.13 and 2.14

--- part 2/data_for_countries/src/components/country_info.js
+++ part 2/data_for_countries/src/components/country_info.js
@@ -1,4 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Weather = ({ location }) => {
+    const [weather, setWeather] = useState({})
+    useEffect(
+        () => {
+            const api_key = process.env.REACT_APP_WEATHER_API_KEY
+            const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${location}`
+            console.log(url);
+            axios
+                .get(url)
+                .then(response => {
+                    setWeather(response.data.current)
+                    return console.log(response.data.current)
+                })
+        },
+        [location])
+    return <div>
+        <p><strong>Observed Time:</strong> {weather.observation_time}</p>
+        <p><strong>Temperature:</strong> {weather.temperature} degrees celsius</p>
+        <img src={weather.weather_icons} alt={`icon for weather in ${location}`} />
+        <p><strong>Wind:</strong> {weather.wind_speed} mph, direction {weather.wind_dir}, {weather.wind_degree} degrees</p>
+    </div>
+}
 
 const CountryInfo = ({ country }) => {
     return <div>
@@ -12,6 +36,8 @@ const CountryInfo = ({ country }) => {
             )}
         </ul>
         <img src={country.flag} alt={`flag of ${country.name}`} style={{ width: 400, height: 200, resizeMode: "contain" }} />
+        <h2>Weather in {country.capital}</h2>
+        <Weather location={country.capital} />
     </div>
 }
 
--- part 2/data_for_countries/src/components/country_list.js
+++ part 2/data_for_countries/src/components/country_list.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CountryInfo from './country_info'
 
 const CountryList = ({ countryQuery, countries }) => {
-    const countryFilter = (country) => {
-        return country.name.toLowerCase().includes(countryQuery.toLowerCase())
-    }
-    const filteredCountries = countries.filter(countryFilter)
 
+    const [filteredCountries, filterCountry] = useState([])
+
+    useEffect(
+        () => {
+            const countryFilter = (country) => {
+                return country.name.toLowerCase().includes(countryQuery.toLowerCase())
+            }
+            filterCountry(countries.filter(countryFilter))
+        },
+        [countries, countryQuery]
+    )
+
+    const buttonClicked = (name) => {
+        return () => {
+            return filterCountry(filteredCountries.filter(
+                (country) => country.name.toLowerCase() === name.toLowerCase())
+            )
+        }
+    }
+
+    // console.log(countries);
     console.log(filteredCountries);
     if (countryQuery.length !== 0) {
         if (filteredCountries.length === 0) {
@@ -16,7 +33,11 @@ const CountryList = ({ countryQuery, countries }) => {
             return <CountryInfo country={filteredCountries[0]} />
         } else if (filteredCountries.length <= 10) {
             return filteredCountries.map((country) => {
-                return <p key={country.name}>{country.name}</p>
+                return <div key={country.name}>
+                    <p>
+                        {country.name} <button onClick={buttonClicked(country.name)}>Show</button>
+                    </p>
+                </div>
             })
 
         } else {
